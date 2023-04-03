c407101e6e55f0a610e2664f25ce0fa80b9b18d6
Alejandra Landa <aleriquene@gmail.com>
2023-04-03 18:42:56 +0000

    photo create works

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ get("/users/:username",{:controller=>"users", :action=>"show"})
 get("/photos",{:controller=>"photos", :action=>"index"})
 get("/photos/:path_id",{:controller=>"photos", :action=>"show"})
 get("/delete_photo/:toast_id",{:controller=>"photos", :action=>"bye"})
+get("/insert_photo",{:controller=>"photos", :action=>"create"})
 end
