58768ae6a3196c1b4ee99998bf6e1f0353080dbc
Alejandra Landa <aleriquene@gmail.com>
2023-04-03 16:22:17 +0000

    added the username route, and worked on the user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
     @list_of_users = matching_user.order({:username => :asc})
     render({:template=>"user_templates/index.html.erb"})
   end
+  def show
+  url_username = params.fetch("username")
+  matching_usernames = User.where({:username=>url_username})
+  @the_user = matching_usernames.first
+  # if the_user == nil
+  #   redirect_to("/")
+  # else
+    render({:template=>"user_templates/show.html.erb"})
+  # end
+  end
 end
