934343bdb4e3c2b2f758bf8721d290cf7a20b190
Alejandra Landa <aleriquene@gmail.com>
2023-04-03 16:41:29 +0000

    photos route and page completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 get("/users",{:controller=>"users", :action=>"index"})
 get("/users/:username",{:controller=>"users", :action=>"show"})
 
+get("/photos",{:controller=>"photos", :action=>"index"})
+get("/photos/:path_id",{:controller=>"photos", :action=>"show"})
 end
